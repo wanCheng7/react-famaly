147d0ebf5ba21c9db5f4f15e948f5ec3b7c09cfb
wancheng <525918717@qq.com>
2019-01-16 18:41:11 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import getRouter from './router/router';
+
+ReactDom.render(
+  getRouter(),
+  document.getElementById('app')
+)
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class extends Component {
+  render() {
+    return (
+      <div>
+        this is home7
+      </div>
+    )
+  }
+}
