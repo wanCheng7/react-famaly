de24450157372660b04ae594bbba863755493dcc
wancheng <525918717@qq.com>
2019-01-17 18:14:48 +0800

    reat-hot-loader

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {AppContainer} from 'react-hot-loader';
+
 import getRouter from './router/router';
 
+/*初始化*/
+renderWithHotReload(getRouter());
+
+/*热更新*/
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept('./router/router', () => {
+    const getRouter = require('./router/router').default;
+    renderWithHotReload(getRouter());
+  });
 }
 
-ReactDom.render(
-  getRouter(),
-  document.getElementById('app')
-)
+function renderWithHotReload(RootElement) {
+  ReactDom.render(
+      <AppContainer>
+          {RootElement}
+      </AppContainer>,
+      document.getElementById('app')
+  )
+}
+
+// ReactDom.render(
+//   getRouter(),
+//   document.getElementById('app')
+// )
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 module.exports = {
   /*入口*/
-  entry: path.join(__dirname, 'src/index.js'),
+  entry: [
+    'react-hot-loader/patch',
+    path.join(__dirname, 'src/index.js')
+  ],
 
   /* 模块文件 */
   module: {
